1096106c9d0d2c70d3cf6d2c5347799243eb670d
Greg Laabs <OverloadUT@gmail.com>
2014-04-22 11:06:39 -0700

    Fixed demo mode script again

--- python/demomode.py
+++ python/demomode.py
@@ -3,18 +3,26 @@ from autofinger import Arduino
 from random import randint
 from time import sleep
 
+colors = [
+    "255000000",
+    "255255000",
+    "000255000",
+    "000255255",
+    "000000255",
+    "255000255",
+]
+
 def main():
     global arduino
 
     seats_sorted = sorted(seats, key=lambda key: key)
+    print seats_sorted
     while True:
         for seatkey in seats_sorted:
             print seatkey
-            commands = []
-            commands.append("allinone{0:03d}{1:03d}{2}".format(seats[seatkey][0], seats[seatkey][1], colors[randint(0,5)]));
+            command = "allinone{0:03d}{1:03d}{2}".format(seats[seatkey][0], seats[seatkey][1], colors[randint(0,5)]);
 
-            for command in commands:
-                arduino.send(command)
+            arduino.send(command)
             sleep(1.2)
 
 
@@ -28,6 +36,7 @@ if __name__ == '__main__':
     finally:
         print "Resetting finger to center position..."
         sleep(2)
-        arduino.returntocenter()
+        arduino.force_reset()
         print "Bye!"
+        raise
         exit()
